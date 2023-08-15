406a75eb0fcd44048e30395985b7f764efcc3cc6
nisitin <seiya110307@icloud.com>
2023-08-15 19:55:50 +0900

    feat: 各種Providerを設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello Router!</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
