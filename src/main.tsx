fb95a8bdf79864bf2905e380d993df8a9ba1e5ad
nisitin <seiya110307@icloud.com>
2023-08-16 17:47:45 +0900

    feat: ルーティングを記載

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { Root } from './routes/root'
+import { SignIn } from './routes/signIn'
+import { SignUp } from './routes/signUp'
 
+// これはあくまでも視覚化するもの
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello Router!</div>,
+    element: <Root />,
+    children: [
+      {
+        path: 'signin',
+        element: <SignIn />,
+      },
+      {
+        path: 'signup',
+        element: <SignUp />,
+      },
+    ],
   },
 ])
 
