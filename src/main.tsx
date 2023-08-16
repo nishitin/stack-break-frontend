e4f5e62ecaa8bfd90281dd3f6de72be401f67861
nisitin <seiya110307@icloud.com>
2023-08-16 20:44:11 +0900

    feat: ルーティングの変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import { Root } from './routes/root'
-import { SignIn } from './routes/signIn'
-import { SignUp } from './routes/signUp'
-
-// これはあくまでも視覚化するもの
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-  },
-  {
-    path: 'signin',
-    element: <SignIn />,
-  },
-  {
-    path: 'signup',
-    element: <SignUp />,
-  },
-])
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
 )
